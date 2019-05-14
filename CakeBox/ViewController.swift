5eedc778f8352a6df82b69241001bc64aec3d32c
Vineet Ramisetty <vineetr@uw2-121-20.local>
2019-05-13 21:12:41 -0700

    We did a thing

--- CakeBox/ViewController.swift
+++ CakeBox/ViewController.swift
@@ -12,14 +12,31 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.view.backgroundColor = UIColor.white
         let title = UILabel()
         title.text = "Honorary Chocolate Cake"
         title.font = UIFont(name: "Avenir", size: 24)
-        title.center = CGPoint(x: 160, y: 28)
         title.sizeToFit()
+        title.center = CGPoint(x: self.view.center.x,
+                               y: 64)
+       
+        let cakeImage = UIImageView(image: UIImage(named: "Honorary Chocolate Cake"))
+        cakeImage.frame = CGRect(x: 16, y: 128, width: self.view.bounds.width - 32, height: 233)
+        print(self.view.bounds.width)
+        
+        let cakeDescription = UITextView()
+        cakeDescription.text = "This is an Honorary Chocolate Cake to celebrate the making of this app. This cake is super delicious and is hehehehe hohohohoho who whow hwoooo whooo he hoow hoe how ehuwh nene enene enene nene nean ean ean enen ytuijis sdof odhfgo jsd ajd fdsiugous fhudshofuhou gusdfu uu dduduc nsadagaz aza fhu sh si aii oauh  asdfjkl; "
+        cakeDescription.font = UIFont(name: "Avenir", size: 16)
+        cakeDescription.frame = CGRect.init(x: 16, y: 375, width: self.view.bounds.width - 32, height: 100)
+        cakeDescription.sizeToFit()
+        cakeDescription.textAlignment = .justified
+//        cakeDescription.sizeToFit()
+        //cakeDescription.center = CGPoint(x: self.view.center.x,
+                                //         y: 450)
         
         self.view.addSubview(title)
+        self.view.addSubview(cakeImage)
+        self.view.addSubview(cakeDescription)
         
     }
 
