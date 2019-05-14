aed8c38b43d736e1d1bbdfde95a02316256aa7b4
Vineet Ramisetty <vineetr@uw2-140-07.local>
2019-05-14 15:12:43 -0700

    Extracted functionality from viewDidLoad

--- CakeBox/CakePickerViewController.swift
+++ CakeBox/CakePickerViewController.swift
@@ -13,29 +13,30 @@ class CakePickerViewController: UIViewController {
     private var db: [[String]] = []
     private var pageIndex: Int = 0
     
+    var cakeTitle: UILabel = UILabel()
+    let cakeImageView = UIImageView()
+    let cakeDescription = UITextView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = UIColor.white
         
         db = readPropertyList(bundleName: "Cakes")
         
-        let title = UILabel()
-        title.text = db[pageIndex][0]
-        title.font = UIFont(name: "Avenir",
+        getPageDetails()
+        
+        cakeTitle.font = UIFont(name: "Avenir",
                             size: 24)
-        title.sizeToFit()
-        title.center = CGPoint(x: self.view.center.x,
+        cakeTitle.sizeToFit()
+        cakeTitle.center = CGPoint(x: self.view.center.x,
                                y: 64)
-       
-        let cakeImage: UIImage = UIImage(named: db[pageIndex][1])!
-        let cakeImageView = UIImageView(image: cakeImage)
+        
         cakeImageView.frame = CGRect(x: 16,
                                      y: 128,
                                      width: self.view.bounds.width - 32,
                                      height: 233)
         
-        let cakeDescription = UITextView()
-        cakeDescription.text = db[pageIndex][2]
+        
         cakeDescription.isEditable = false
         cakeDescription.font = UIFont(name: "Avenir",
                                       size: 16)
@@ -46,7 +47,8 @@ class CakePickerViewController: UIViewController {
         cakeDescription.sizeToFit()
         cakeDescription.textAlignment = .justified
         
-        self.view.addSubview(title)
+        
+        self.view.addSubview(cakeTitle)
         self.view.addSubview(cakeImageView)
         self.view.addSubview(cakeDescription)
     }
@@ -57,14 +59,24 @@ class CakePickerViewController: UIViewController {
             if let cakeFromPlist = NSArray(contentsOf: URL) as? [[String]] {
                 cakes = cakeFromPlist
             }
+
         }
         
         return cakes
     }
     
+    func getPageDetails(){
+        cakeTitle.text = db[pageIndex][0]
+        cakeImageView.image = UIImage(named: db[pageIndex][1])!
+        cakeDescription.text = db[pageIndex][2]
+    }
+    
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
         print("position \(touches.first!.location(in: self.view))")
-        print("touches eneded")
+        print("touches ended")
+        pageIndex += 1
+        pageIndex %= db.count
+        getPageDetails()
     }
     
 }
